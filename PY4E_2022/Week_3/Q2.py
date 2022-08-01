bc8ef5228a0608956bdc40bceb0ef5b34ae0455a
Sungkuk Park <sungkuk.park@wooga.net>
2022-08-01 11:53:52 +0900

    anwer to question 2

--- PY4E_2022/Week_3/Q2.py
+++ PY4E_2022/Week_3/Q2.py
@@ -19,17 +19,44 @@ import random
 # 진입 함수
 def main():
     run_tests()
-    run_game()
+
+    ## 조건 1: 게임을 몇 판을 진행할 것인지 입력을 받아주기
+    playerWin = npcWin = 0
+    sessions = int(input("몇 판을 진행할 겁니까?: "))
+    for s in range(0, sessions):
+        print("첫 번째 판을 진행합니다:")
+        result = run_game()
+        if result == "플레이어 승리":
+            playerWin += 1
+        elif result == "컴퓨터 승리":
+            npcWin += 1
+
+    ## 조건 3: 게임종료 후 나와 컴퓨터의 총 전적 출력하기
+    print(f"총 플레이 횟수: {sessions}, 플레이어 승: {playerWin}, 컴퓨터 승: {npcWin}")
 
 
 # 게임 함수
 def run_game():
-    playerInput = input("플레이어 입력: ")
+    ## 조건 2: 0, 1, 2, "가위", "바위", "보" 이외에 다른 입력을 받으면 재입력 받기
+    success = False
+    while not success:
+        playerInput = input("플레이어 입력: ")
+        if not is_in_input_range(playerInput):
+            print(f"\"{playerInput}\"은 정상적인 입력이 아닙니다. 다시 입력해주세요.")
+            print("(\"가위\", \"바위\", \"보\" 가운데 하나의 입력을 필요로 합니다.)")
+            continue
+        success = True
+
     npcInput = generate_npm_input()
     result = rock_scissor_paper(playerInput, npcInput)
     print(f'플레이어는 "{playerInput}"를 냈습니다.')
     print(f'컴퓨터는 "{npcInput}"를 냈습니다.')
     print(f'결과는 "{result}"입니다.')
+    return result
+
+
+def is_in_input_range(input):
+    return input == "가위" or input == "바위" or input == "보"
 
 
 # 테스트 함수
